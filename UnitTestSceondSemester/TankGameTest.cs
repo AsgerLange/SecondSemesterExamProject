3ea607bbed3e7a4c29f89f4cec456e4a01c6a465
Johannes Andersen <defe@live.dk>
2018-05-17 13:02:30 +0200

    Unit testing methods setup

---
+++ UnitTestSceondSemester/TankGameTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TankGame;
+
+namespace UnitTestSceondSemester
+{
+    [TestClass]
+    public class TankGameTest
+    {
+        //private test;
+
+        //[TestInitialize]
+        //public void TestStart()
+        //{
+        //    test = new TankGame();
+        //}
+
+        [TestMethod]
+
+        public void AddComponent() //Test if componenets are added... maybe
+        {
+
+        }
+
+        public void EnemyTakesDamage()
+        {
+            
+
+        }
+        public void PlayerTakesDamage()
+        {
+
+
+        }
+        public void TowerTakesDamage()
+        {
+
+
+        }
+        public void PlayerShoot()
+        {
+
+
+        }
+        public void BulletDamage() //Test if the damage of the bullet does that amount
+        {
+
+        }
+        public void TowerView() //Test if the towers can see the things around them.
+        {
+
+
+        }
+
+        public void TowerShoot()
+        {
+
+
+        }
+
+        public void EnemyDoDamage() //Test if enemy lowers the health of player to the amount that we know
+        {
+
+
+        }
+        public void Collision() //To test if collision happends
+        {
+
+
+        }
+        public void EnemyView() //For testing if enemy see's something that they will move towards it
+        {
+
+
+        }
+        public void BuyTower()
+        {
+
+
+        }
+        public void RandomWeapon() //Check what weapon the player got from a random between some numbers
+        {
+
+
+        }
+        public void RandomGoldAmount() //Check the amount of gold from SupplyCrate between some numbers.
+        {
+
+
+        }
+
+    }
+}
