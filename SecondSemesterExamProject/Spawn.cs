1061447adc0983cf2ca423b0e001735339d116fb
Sten4321 <1357913579ty@gmail.com>
2018-05-22 12:08:57 +0200

    Spawn prep

---
+++ SecondSemesterExamProject/Spawn.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankGame
+{
+    class Spawn
+    {
+        private int wave = 0;
+        private int waveSize = 0;
+        private int spawned = 0;
+        private Random rnd = new Random();
+
+        /// <summary>
+        /// returns the current wave number
+        /// </summary>
+        public int Wave
+        {
+            get { return wave; }
+        }
+
+        /// <summary>
+        /// returns the size of the current wave
+        /// </summary>
+        public int WaveSize
+        {
+            get { return waveSize; }
+        }
+
+        /// <summary>
+        /// Prepers the waveSpavner
+        /// </summary>
+        public Spawn()
+        {
+
+        }
+
+        /// <summary>
+        /// tjecks and spawns new waves
+        /// </summary>
+        public void Update()
+        {
+
+        }
+
+        /// <summary>
+        /// creates a new wave
+        /// </summary>
+        private void CreateWave()
+        {
+
+        }
+    }
+}
