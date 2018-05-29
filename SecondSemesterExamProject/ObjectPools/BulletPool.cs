18dc96ce709a6c3f18f402b387b9d4ff4541af0d
Sten4321 <1357913579ty@gmail.com>
2018-05-29 09:57:18 +0200

    Bullet hit fix

--- SecondSemesterExamProject/ObjectPools/BulletPool.cs
+++ SecondSemesterExamProject/ObjectPools/BulletPool.cs
@@ -49,6 +49,7 @@ namespace TankGame
                         {
                             if (((Bullet)comp).GetBulletType == bulletType)
                             {
+
                                 tmp = bul;
                                 break;
                             }
